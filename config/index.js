880316b0ab419de16ad4eae807d2f3f49391d4de
hahahhhhhhkdjcvdusg <112570601+hahahhhhhhkdjcvdusg@users.noreply.github.com>
2022-09-01 00:02:42 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -13,7 +13,7 @@ export const config = {
     TEMPLATE_ID: "nxdJy8O8yvW_8RCNM1JHkEHBas5h5rYxfevX8aTJPOQ",
 
     // 回调消息模板id, 用来看自己有没有发送成功的那个模板
-    CALLBACK_TEMPLATE_ID: "xxxxxxxxxxxxx-y你懂的,填错狗头打烂VRMpu",
+    CALLBACK_TEMPLATE_ID: "nxdJy8O8yvW_8RCNM1JHkEHBas5h5rYxfevX8aTJPOQ",
 
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如
     // [{
@@ -32,7 +32,7 @@ export const config = {
     ],
     // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
     CALLBACK_USERS: [
-      {name: '自己',id: "5hxOO6VskYv你懂的,填错狗头打烂-ywIZVRMpuXhk"}, 
+      {name: '自己',id: "oiBeO6Sr0aL3l20xObtC8VN6fyrE"}, 
     ],
      
     /**
@@ -42,9 +42,9 @@ export const config = {
     /** 天气相关 */
 
     // 所在省份
-    PROVINCE: "广东",
+    PROVINCE: "山东",
     // 所在城市
-    CITY: "惠州",
+    CITY: "烟台",
 
     /** 重要节日相关 */
 
