0e4b7c2a3e4ba25302d95c009d0908b38ae8c5f1
hahahhhhhhkdjcvdusg <112570601+hahahhhhhhkdjcvdusg@users.noreply.github.com>
2022-09-01 01:31:07 +0800

    Update index.js

--- src/services/index.js
+++ src/services/index.js
@@ -141,9 +141,9 @@ export const getBirthdayMessage = () => {
         const age = dayjs().diff(item.year + '-' + item.date, 'year');
   
         if (item.diffDay === 0) {
-          message = `今天是 ${item.name} 的${age ? age + '岁' : ''}生日哦，祝${item.name}生日快乐！`
+          message = `今天是 ${item.name} 的 ${age ? age + '岁' : ''} 生日哦，祝${item.name}生日快乐！`
         } else {
-          message = `距离 ${item.name} 的${age ? age + 1 + '岁' : ''}生日还有${item.diffDay}天`
+          message = `距离 ${item.name} 的 ${age ? age + 1 + '岁' : ''} 生日还有${item.diffDay}天`
         }
       }
 
